aa2fe6a27d3762ca6abd166e2f553b999665d144
alexgdin <alexandrudin38@yahoo.com>
2026-09-17 00:43:12 +0300

    Added Main Menu (not fully functional) and first demo of ray

--- Assets/Scripts/light_source.cs
+++ Assets/Scripts/light_source.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float defDistanceRay = 100;
+    
+    public Transform m_lightSource;
+    public LineRenderer m_lineRenderer;
+    
+    Transform m_transform;
+
+
+
+    private void Awake()
     {
-        
+        m_transform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ThrowRay();
+    }
+
+    void ThrowRay()
+    {
+        if (Physics2D.Raycast(m_transform.position, m_transform.right, defDistanceRay))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(m_transform.position, m_transform.right, defDistanceRay);
+            Draw2DRay(m_transform.position, hit.point);
+        }
+        else
+        {
+            Draw2DRay(m_lightSource.position, m_lightSource.position + m_lightSource.right * defDistanceRay);
+        }
+    }
+
+    void Draw2DRay(Vector2 start, Vector2 end)
+    {
+        m_lineRenderer.SetPosition(0, start);
+        m_lineRenderer.SetPosition(1, end);
     }
 }
